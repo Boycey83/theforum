b5a75e1bacfc7af177e805442cf9cafa163b6785
Boycey83 <alanboyce@hotmail.com>
2024-01-05 21:54:27 +0000

    TF-3 Implement a way of injecting styles...
    into the css from the backend. Just the background colour for now as a
    test, but the rest will follow and we can theme the application based on
    environment variables / settings.

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using theforum.BusinessLogic;
 using theforum.Resources;
+using theforum.Settings;
 using theforum.ViewModels;
 
 namespace theforum.Controllers;
@@ -11,24 +12,26 @@ public class HomeController : Controller
 {
     private readonly UserAccountService _userAccountService;
     private readonly ForumResources _forumResources;
+    private readonly StyleSettings _styleSettings;
 
     public HomeController(UserAccountService userAccountService, IOptions<ForumResources> forumResources)
     {
         _userAccountService = userAccountService;
         _forumResources = forumResources.Value;
+        _styleSettings = new StyleSettings { BodyBgColor = "#262729" };
     }
 
     [HttpGet]
     public IActionResult Index() => 
-        View(new ForumViewModel(_forumResources));
+        View(new ForumViewModel(_forumResources, _styleSettings));
 
     [HttpGet("Thread/{threadId:int}")]
     public IActionResult Thread(int threadId) => 
-        View("Index", new ForumViewModel(_forumResources, threadId));
+        View("Index", new ForumViewModel(_forumResources, _styleSettings, threadId));
 
     [HttpGet("Thread/{threadId:int}/Reply/{replyId:int}")]
     public IActionResult Reply(int threadId, int replyId) => 
-        View("Index", new ForumViewModel(_forumResources, threadId, replyId));
+        View("Index", new ForumViewModel(_forumResources, _styleSettings, threadId, replyId));
 
     [HttpGet("UserAccount/{userAccountId:int}/Confirm/{tokenString}")]
     public IActionResult ConfirmUserAccount(int userAccountId, string tokenString)
---
+++ src/Settings/StyleSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace theforum.Settings;
+
+public record StyleSettings
+{
+    public string BodyBgColor { get; init; } = string.Empty;
+}
