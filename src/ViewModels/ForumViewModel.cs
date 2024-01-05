4bdce8473dfc2b134f3988c0a8b1eed0684f390a
Boycey83 <alanboyce@hotmail.com>
2024-01-05 21:54:27 +0000

    TF-3 Implement a way of injecting styles...
    into the css from the backend. Just the background colour for now as a
    test, but the rest will follow and we can theme the application based on
    environment variables / settings.

--- src/ViewModels/ForumViewModel.cs
+++ src/ViewModels/ForumViewModel.cs
@@ -1,6 +1,9 @@
+using theforum.Settings;
+
 namespace theforum.ViewModels;
 
 public record ForumViewModel(
     Resources.ForumResources Resources, 
+    StyleSettings StyleSettings,
     int? ThreadId = null, 
     int? ReplyId = null);
