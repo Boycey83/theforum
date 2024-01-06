c829ad0aec4334f5fb4730b55d96f96e721dad43
Boycey83 <alanboyce@hotmail.com>
2024-01-06 08:31:10 +0000

    TF-3 Add the ability to override every colour...
    in the theme. The next step will be to draw these colours from the app
    settings and the environment variables.

--- src/Settings/StyleSettings.cs
+++ src/Settings/StyleSettings.cs
@@ -1,6 +1,38 @@
-﻿namespace theforum.Settings;
+﻿using Microsoft.AspNetCore.Html;
+
+namespace theforum.Settings;
 
 public record StyleSettings
 {
-    public string BodyBgColor { get; init; } = string.Empty;
+    public string PrimaryBgColor { get; init; } = string.Empty;
+    public string PrimaryAccentColor { get; init; } = string.Empty;
+    public string SecondaryAccentColor { get; init; } = string.Empty;
+    public string BodyTextColor { get; init; } = string.Empty;
+    public string DarkTextColor { get; init; } = string.Empty;
+    public string SectionBgColor { get; init; } = string.Empty;
+    public string DividerColor { get; init; } = string.Empty;
+    public string NeutralAccentColor { get; init; } = string.Empty;
+    public string SoftDividerColor { get; init; } = string.Empty;
+    public string LightAccentColor { get; init; } = string.Empty;
+    public string MutedAccentColor { get; init; } = string.Empty;
+    public string DarkSectionBgColor { get; init; } = string.Empty;
+
+    public IHtmlContent AsCss =>
+        new HtmlString(
+            $$"""
+              :root {
+                  --primary-accent-color: {{PrimaryAccentColor}};
+                  --secondary-accent-color: {{SecondaryAccentColor}};
+                  --body-text-color: {{BodyTextColor}};
+                  --dark-text-color: {{DarkTextColor}};
+                  --section-bg-color: {{SectionBgColor}};
+                  --primary-bg-color: {{PrimaryBgColor}};
+                  --divider-color: {{DividerColor}};
+                  --neutral-accent-color: {{NeutralAccentColor}};
+                  --soft-divider-color: {{SoftDividerColor}};
+                  --light-accent-color: {{LightAccentColor}};
+                  --muted-accent-color: {{MutedAccentColor}};
+                  --dark-section-bg-color: {{DarkSectionBgColor}};
+              }
+              """);
 }
